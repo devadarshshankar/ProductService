f9507a1b9cb206578e250dc2bddf0d2856c92e6a
adarsh shankar <adarshshankarofficial@gmail.com>
2024-08-11 17:04:08 +0530

    Added spring security OAuth2 resource server

---
+++ src/main/java/dev/adarsh/productservice/security/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package dev.adarsh.productservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.oauth2.core.authorization.OAuth2AuthorizationManagers.hasScope;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        //.requestMatchers("/messages/**").access(hasScope("message:read"))
+                        .anyRequest().authenticated()//only allow a person who has logged in
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+      //  .anyRequest().permitAll() -> allow anyone to access the url without login
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(jwt -> jwt
+//                                .jwtAuthenticationConverter(myConverter())
+//                        )
+//                );
+        return http.build();
+    }
+}
